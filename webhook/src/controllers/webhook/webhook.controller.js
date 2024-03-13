e2dc50add42f20050248c81c079a9880b5f1c6a3
DSmith575 <SMITDE5@student.op.ac.nz>
2024-03-13 20:59:29 +1300

    perf(component): added early return status to webhook
    will return 400(Bad request) with empty payload message if webhook returns {}

--- webhook/src/controllers/webhook/webhook.controller.js
+++ webhook/src/controllers/webhook/webhook.controller.js
@@ -4,7 +4,16 @@ import { STATUS_CODES } from "../../utils/statusCodes/statusCode.js";
 export const handleWebhook = async (req, res) => {
 	try {
 		const payload = req.body;
+
+		// Remove later
 		console.log(payload);
+
+		if (!payload || Object.keys(payload).length === 0) {
+			return res.status(STATUS_CODES.BAD_REQUEST).json({
+				statusCode: res.statusCode,
+				message: "Empty payload received",
+			});
+		};
 
 		return res.status(STATUS_CODES.OK).json({
 			statusCode: res.statusCode,
@@ -14,8 +23,7 @@ export const handleWebhook = async (req, res) => {
 	} catch (error) {
 		return res.status(STATUS_CODES.SERVER_ERROR).json({
 			statusCode: res.statusCode,
-			message:
-				"Internal server error occurred while processing the webhook request",
+			message: "Internal server error occurred while processing the webhook request",
 		});
 	}
 };
