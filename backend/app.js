629e37ccdea625cc53c9a5732fdfce07e6a60773
Joshua McDonough (1000107941) <MCDOJL4@student.op.ac.nz>
2024-03-14 13:23:42 +1300

    Added webhook test to app.js

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,15 @@ app.use(cors());
 // Use the routes module
 app.use('/test', test);
 
+app.post('https://webhooktest-6o78.onrender.com/api/v1/integration/webhook', (req, res) => {
+    const payload = req.body;
+    const uplink = payload.uplink_message;
+    res.status(200).send('Webhook received successfully.');
+    return res.json({
+        uplink
+    });
+})
+
 // Start the server on port 3000
 app.listen(3000, () => {
   console.log('Server is listening on port 3000.');
