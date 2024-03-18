185168ebffc0e70856a794cd20e7f9a70c76326b
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2024-03-18 13:47:29 +1300

    test(component): split previous tests into more tests to throughly check returns"

--- webhook/src/test/webhook.test.js
+++ webhook/src/test/webhook.test.js
@@ -26,28 +26,122 @@ describe('POST webhook', () => {
     afterAll(() => {
         server.close();
     })
+
     it('should respond with 400 Bad request when payload is empty', async () => {
         const response = await request(app)
         .post(webhookPath)
-        .send({});
-
         expect(response.status).toBe(400);
-        expect(response.statusCode).toBe(400);
-        expect(response.body.message).toBe("Empty payload received");
     });
 
-    it('should return payload data', async () => {
+    it('should respond with an object containing a statusCode of 400 when the payload is empty', async () => {
+        const response = await request(app)
+        .post(webhookPath)
+        .send({})
+        const { statusCode } = response.body;
+        expect(statusCode).toBe(400)
+    })
+
+    it('should respond with an object containing a message of Empty payload received when the payload sent is empty', async () => {
+        const response = await request(app)
+        .post(webhookPath)
+        .send({})
+        const { message } = response.body;
+        expect(message).toBe('Empty payload received')
+    })
+
+    it('should return a status of 200 when POST receives data', async () => {
         const response = await request(app)
         .post(webhookPath)
         .send(payload)
-
-        console.log(response.body);
         expect(response.status).toBe(200);
-        expect(response.statusCode).toBe(200);
-        expect(response.body.message).toBe("Payload received");
-        expect(response.body.data).toEqual(payload);
-        expect(response.body.data.end_device_ids.device_id).toEqual('00d3c69800bff312')
     })
+
+    it('should respond with an object containing a message with a statusCode of 200', async () => {
+        const response = await request(app)
+        .post(webhookPath)
+        .send(payload)
+        const { statusCode } = response.body;
+        expect(statusCode).toBe(200)
+    })
+
+    it('should respond with an object containing a message displaying Payload received', async () => {
+        const response = await request(app)
+        .post(webhookPath)
+        .send(payload)
+        const { message } = response.body;
+        expect(message).toBe("Payload received")
+    })
+
+    it('should respond with a payload containing the data within the init object {payload}', async () => {
+        const response = await request(app)
+        .post(webhookPath)
+        .send(payload)
+        const { data } = response.body;
+        expect(data).toEqual(payload)
+    })
+
+    it('should respond with a payload containing the data decoded_payload which contains the co2 and temperature', async () => {
+        const response = await request(app)
+        .post(webhookPath)
+        .send(payload)
+        // destructure incoming payload data: uplink_message: { decoded_payload }
+        const { data } = response.body;
+        const { uplink_message } = data;
+        expect(uplink_message.decoded_payload.receivedString).toBe('400:22\x00\x00')
+    })
+
+    it('should respond with a payload containing the data end_device_ids which contains the device_id', async () => {
+        const response = await request(app)
+        .post(webhookPath)
+        .send(payload)
+        const { data } = response.body;
+        const { end_device_ids } = data;
+        expect(end_device_ids.device_id).toBe('00d3c69800bff312')
+    })
+
+    it('should respond with a payload containing the data application_ids which contains the id co2-test', async () => {
+        const response = await request(app)
+        .post(webhookPath)
+        .send(payload)
+        const { data } = response.body;
+        const { end_device_ids } = data;
+        expect(end_device_ids.application_ids.application_id).toBe('co2-test')
+    })
+
 });
+
+describe('GET endpoints', () => {
+    afterAll(() => {
+        server.close();
+    })
+
+    it('should respond with a 200 OK', async () => {
+        const response = await request(app)
+        // get the root path 
+        .get('/')
+        expect(response.status).toBe(200)
+    })
+
+    it('should return an object containing a statusCode of 200', async () => {
+        const response = await request(app)
+        .get('/')
+        const { statusCode } = response.body;
+        expect(statusCode).toBe(200)
+    })
+
+    it('should return a message displaying Available endpoints', async () => {
+        const response = await request(app)
+        .get('/')
+        const { message } = response.body;
+        expect(message).toBe("Available endpoints")
+    })
+
+    it('should return an object containing an endpoint of [POST]: /api/v1/....', async () => {
+        const response = await request(app)
+        .get('/')
+        const { endpoints } = response.body;
+        expect(endpoints).toBe('[POST]: /api/v1/integration/webhook')
+    })
+})
 
 
