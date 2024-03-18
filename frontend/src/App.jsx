c25e6a7892da73433e636ba2fcbc4aa035f9a7db
declandeh00 <104808190+declandeh00@users.noreply.github.com>
2024-03-18 13:18:38 +1300

    Chore(App.jsx): Fixed the code to go with the code formatting we have in the code standards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Co2Level from './Component/Co2Level'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
    <>
     <Co2Level />
