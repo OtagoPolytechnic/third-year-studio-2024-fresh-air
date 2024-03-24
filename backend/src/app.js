38cc7e05e763f78a02df98740bfbf63313a5a73d
DSmith575 <SMITDE5@student.op.ac.nz>
2024-03-24 20:21:23 +1300

    feat(component): moved webhook into backed
    added more dev dependencies (prettier, etc) and normal dependencies (Axios, helmet, etc)

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import { INDEX_PATHS, PORTS } from './utils/constants/globalConstants.js';
+import { STATUS_CODES } from './utils/statusCodes/statusCode.js';
+import webhook from './routes/webhook/webhook.route.js';
+
+const port = PORTS.SERVER_PORT;
+// basePath sets up the /api/v1 endpoint
+const basePath = `/${INDEX_PATHS.BASE_URL}/${INDEX_PATHS.CURRENT_VERSION}`;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+
+app.use(`${basePath}/integration`, webhook);
+
+app.get('/', (req, res) => {
+  return res.status(STATUS_CODES.OK).json({
+    statusCode: res.statusCode,
+    message: 'Available endpoints',
+    endpoints: `[POST]: ${basePath}/integration/webhook`,
+  });
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export { app, server };
