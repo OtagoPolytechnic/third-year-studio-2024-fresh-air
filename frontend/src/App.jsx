a3c3047712287449a00ef22036dcba5943fbf78c
declandeh00 <104808190+declandeh00@users.noreply.github.com>
2024-04-03 01:27:52 +1300

    refactor(App.jsx): Changed it so that it uses the Co2Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Co2Level from './Component/Co2Level'
+import Co2Home from './Component/Co2Home'
 
 export const App = () => {
   return (
    <>
-    <Co2Level />
+    <Co2Home />
    </>
   )
 }
