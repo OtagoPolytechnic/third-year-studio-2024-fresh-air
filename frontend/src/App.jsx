dc1b0757c94b0342b71e4cdda6e24e0623e585bb
Joshua McDonough (1000107941) <MCDOJL4@student.op.ac.nz>
2024-04-04 14:11:32 +1300

    Removed second export

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Co2Level from './Component/Co2Level'
 import SensorHistory from './Component/SensorHistory'
 
-export const App = () => {
+const App = () => {
   return (
    <>
     <Co2Level />
@@ -11,4 +11,4 @@ export const App = () => {
   )
 }
 
-export default App
+export default App;
