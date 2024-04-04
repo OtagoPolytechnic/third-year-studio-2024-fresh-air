8b090cf61062dfe9bd2fc7487d29080c49823e80
declandeh00 <104808190+declandeh00@users.noreply.github.com>
2024-04-04 14:23:33 +1300

    Chore(All files): This to help fix the change with the merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import Co2Home from './Component/Co2/Co2Home'
+import Co2Level from './Component/Co2Level'
+import Co2Home from "./Component/Co2/Co2Home"
 
 export const App = () => {
   return (
    <>
     <Co2Home />
+    <Co2Level />
    </>
   )
 }
