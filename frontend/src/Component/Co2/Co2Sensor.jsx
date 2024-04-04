8d435f8cbf824552ab71f49c99b7d4b5813d943e
declandeh00 <104808190+declandeh00@users.noreply.github.com>
2024-04-04 13:16:14 +1300

    Chore(Co2Sensor.jsx): I have fixed the UseEffect to remove the calls and removed the excess export

---
+++ frontend/src/Component/Co2/Co2Sensor.jsx
@@ -0,0 +1,51 @@
+// Co2Sensor.js
+import React, { useState, useEffect } from "react";
+import { Chart } from "react-google-charts";
+
+const getData = (room_nu, co2) => {
+  return [
+    ["Label", "Value"],
+    [room_nu, parseInt(co2)], // Convert co2 to integer
+  ];
+}
+
+const options = {
+  width: 700,
+  height: 400,
+  redFrom: 1500,
+  redTo: 3000,
+  yellowFrom: 1000,
+  yellowTo: 1500,
+  greenFrom: 400,
+  greenTo: 1000,
+  minorTicks: 10,
+  min: 400,
+  max: 3000
+};
+
+const Co2Sensor = ({ room_number, co2 }) => {
+  const [data, setData] = useState(getData(room_number, co2));
+
+  useEffect(() => {
+    const fetchData = () => {
+      const newData = getData(room_number, co2);
+      setData(newData);
+    }
+
+      if (room_number !== data.room_number || co2 !== data.co2) {
+        fetchData();
+      }
+  }, [room_number, co2]); // Depend on room_number and co2
+
+  return (
+    <Chart
+      chartType="Gauge"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+  );
+}
+
+export default Co2Sensor;
