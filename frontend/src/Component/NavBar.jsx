a47c4a7947b7c4118dee26bd47a0d33a3b09e67a
tracy macaulay-higgan <tracy@mad.geek.nz>
2024-04-22 21:01:18 +1200

    adding simplified navbar

---
+++ frontend/src/Component/NavBar.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import './NavBar.css'
+
+function NavBar() {
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+          <li>
+            <a href="#">Link 2</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
