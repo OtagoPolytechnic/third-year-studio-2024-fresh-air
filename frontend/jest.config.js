e40626e88e999e740f892788ce0d109a2abbec2f
Josh McDonough <mcdojl4@student.op.ac.nz>
2024-05-06 12:38:45 +1200

    Added comments for jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,21 +1,21 @@
 export default {
     transform: {
-      '^.+\\.jsx?$': 'babel-jest',
+      '^.+\\.jsx?$': 'babel-jest', // Used to transform JSX to JS with babel
     },
-    transformIgnorePatterns: ['/node_modules/', "\\.css$"],
-    testMatch: ['**/__tests__/**/*.test.js', '**/__tests__/**/*.test.jsx'],
+    transformIgnorePatterns: ['/node_modules/', "\\.css$"], // Ignore CSS files in transform
+    testMatch: ['**/__tests__/**/*.test.js', '**/__tests__/**/*.test.jsx'], // Finds test files that match the pattern
     moduleNameMapper: {},
-    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
-    testEnvironment: 'jsdom',
+    moduleFileExtensions: ['js', 'json', 'jsx', 'node'], // Allows us to import jsx files
+    testEnvironment: 'jsdom', // Sets it to run tests in jsdom
     globals: {
       'ts-jest': {
         tsconfig: 'tsconfig.json',
       },
     },
     setupFilesAfterEnv: [
-        '@testing-library/jest-dom',
+        '@testing-library/jest-dom', // Sets up Jest environment after loading testing-library
     ],
     moduleNameMapper: {
-      "\\.(css|less)$": "identity-obj-proxy"
+      "\\.(css|less)$": "identity-obj-proxy" // Mocks CSS and LESS imports with identity-obj-proxy
     },
 };  
