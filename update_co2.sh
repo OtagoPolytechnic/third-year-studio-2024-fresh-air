4022294d7a8af2c9e37d90c595b9168c6e562f0e
Ubuntu <IoTData@TestServerIoT.5sf2hktcstuebnwzvtv5mt2aia.bx.internal.cloudapp.net>
2024-05-07 02:03:48 +0000

    build: added docker compose up to upadte script

--- update_co2.sh
+++ update_co2.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo Stopping docker containers if running.
 sudo docker compose down
-echo Removing any
+echo Removing any.
 sudo docker rmi co2-nginx:latest postgres:16.2 frontend-co2-app:latest third-year-studio-2024-fresh-air-backend:latest
+
 echo Updating repo.
 git stash
 git checkout staging
 git pull
+
+echo Creating and Running container.
+sudo docker compose up -d && echo Deploy Successful || echo Deploy Failed
