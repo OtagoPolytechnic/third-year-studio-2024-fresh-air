99e0067fde59dd553dad63fff3f4854a53835458
Joshua McDonough (1000107941) <MCDOJL4@student.op.ac.nz>
2024-05-09 14:53:38 +1200

    Created basic Home Page tests

--- frontend/cypress/e2e/App.cy.jsx
+++ frontend/cypress/e2e/App.cy.jsx
@@ -7,5 +7,11 @@ describe('template spec', () => {
   it('Visits Room D201', () => {
     cy.visit('http://localhost:5173/')
     cy.get('[data-cy="D201"]').click()
+    cy.url().should('eq', 'http://localhost:5173/room/D201')
+  })
+  it('Visits Sensor History', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('[data-cy="SensorHistory"]').click()
+    cy.url().should('eq', 'http://localhost:5173/SensorHistory')
   })
 })
--- frontend/src/Component/Pages/Homepage.jsx
+++ frontend/src/Component/Pages/Homepage.jsx
@@ -9,10 +9,10 @@ export const Homepage = () => {
       {/* This is creating links for the pages to be used being hardcoded */}
         <h1>Welcome to D-Block CO<sub>2</sub> Monitor</h1>
         <li><NavLink to="/room/D201" className="link" data-cy="D201" >Room D201</NavLink></li>
-        <li data-cy="D202"><NavLink to="/room/D202" className="link" >Room D202</NavLink></li>
-        <li data-cy="D207"><NavLink to="/room/D207" className="link" >Room D207</NavLink></li>
-        <li data-cy="D207B"><NavLink to="/room/D207 TD" className="link" >Room D207 TD</NavLink></li>
-        <li data-cy="SensorHistory"><NavLink to="/SensorHistory" className="link" >Sensor History</NavLink></li>
+        <li><NavLink to="/room/D202" className="link" data-cy="D202" >Room D202</NavLink></li>
+        <li><NavLink to="/room/D207" className="link" data-cy="D207" >Room D207</NavLink></li>
+        <li><NavLink to="/room/D207 TD" className="link" data-cy="D207B" >Room D207 TD</NavLink></li>
+        <li><NavLink to="/SensorHistory" className="link" data-cy="SensorHistory" >Sensor History</NavLink></li>
       </>
     );
 }
