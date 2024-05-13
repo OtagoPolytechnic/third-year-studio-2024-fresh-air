22b6b898d601f3ffa714805b4a4507443bb8f6d2
legin8 <maynne1@student.op.ac.nz>
2024-05-14 10:50:09 +1200

    testing: replaced backend CMD command with db push

--- update_co2.sh
+++ update_co2.sh
@@ -2,7 +2,7 @@
 echo Stopping docker containers if running.
 sudo docker compose down
 echo Removing any.
-sudo docker rmi co2-nginx:latest postgres:16.2 frontend-co2-app:latest third-year-studio-2024-fresh-air-backend:latest
+sudo docker rmi co2-nginx:latest postgres:16.2 frontend-co2-app:latest backend-co2-app:latest
 
 echo Updating repo.
 git stash
