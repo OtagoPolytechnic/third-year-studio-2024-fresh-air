ce140d6be869fe793276bb29460ea9033e7f568c
Josh McDonough <mcdojl4@student.op.ac.nz>
2024-05-15 01:18:14 +1200

    Fixed RoomPage test

--- frontend/src/Component/Pages/Roompage.jsx
+++ frontend/src/Component/Pages/Roompage.jsx
@@ -73,6 +73,7 @@ export const RoomPage = () => {
       };
 
     const { roomNumber } = useParams();
+    console.log(roomNumber)
 
      // Filter data based on the roomNumber
     const roomData = data.data.filter(item => item.device.room_number === roomNumber);
--- frontend/src/__tests__/RoomPage.test.jsx
+++ frontend/src/__tests__/RoomPage.test.jsx
@@ -1,89 +1,42 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react";
 import { RoomPage } from "../Component/Pages/Roompage";
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 
 describe("RoomPage component", () => {
   test("renders CO2 room data correctly", () => {
-    // Mocking data
-    const data = {
-      "statusCode": 200,
-      "data": [
-        {
-          "id": 646,
-          "co2": "2040",
-          "temperature": "21",
-          "createdAt": "2024-03-28T00:37:53.236Z",
-          "deviceId": "eui-00d3c59800bdd352",
-          "dev_eui": "00D3C59800BDD352",
-          "device": {
-              "id": 1,
-              "room_number": "D201",
-              "deviceId": "eui-00d3c59800bdd352",
-              "dev_eui": "00D3C59800BDD352",
-              "createdAt": "2024-03-27T09:46:41.851Z"
-          }
-      },
+    // Mocks data
+    const data = [
       {
-          "id": 647,
-          "co2": "1100",
-          "temperature": "21",
-          "createdAt": "2024-03-28T00:37:53.236Z",
-          "deviceId": "eui-00d3c59800bdd352",
-          "dev_eui": "00D3C59800BDD352",
-          "device": {
-              "id": 1,
-              "room_number": "D202",
-              "deviceId": "eui-00d3c59800bdd352",
-              "dev_eui": "00D3C59800BDD352",
-              "createdAt": "2024-03-27T09:46:41.851Z"
-          }
-      },
-      {
-          "id": 648,
-          "co2": "602",
-          "temperature": "21",
-          "createdAt": "2024-03-28T00:37:53.236Z",
-          "deviceId": "eui-00d3c59800bdd352",
-          "dev_eui": "00D3C59800BDD352",
-          "device": {
-              "id": 1,
-              "room_number": "D207",
-              "deviceId": "eui-00d3c59800bdd352",
-              "dev_eui": "00D3C59800BDD352",
-              "createdAt": "2024-03-27T09:46:41.851Z"
-          }
-      },
-      {
-          "id": 649,
-          "co2": "602",
-          "temperature": "21",
-          "createdAt": "2024-03-28T00:37:53.236Z",
-          "deviceId": "eui-00d3c59800bdd352",
-          "dev_eui": "00D3C59800BDD352",
-          "device": {
-              "id": 1,
-              "room_number": "D207 TD",
-              "deviceId": "eui-00d3c59800bdd352",
-              "dev_eui": "00D3C59800BDD352",
-              "createdAt": "2024-03-27T09:46:41.851Z"
-          }
+        "id": 646,
+        "co2": "2040",
+        "temperature": "21",
+        "createdAt": "2024-03-28T00:37:53.236Z",
+        "deviceId": "eui-00d3c59800bdd352",
+        "dev_eui": "00D3C59800BDD352",
+        "device": {
+            "id": 1,
+            "room_number": "D201",
+            "deviceId": "eui-00d3c59800bdd352",
+            "dev_eui": "00D3C59800BDD352",
+            "createdAt": "2024-03-27T09:46:41.851Z"
+        }
       }
-      ]
-    };
+    ];
 
-    // Renders the RoomPage component with the mock data
-    const { container } = render(<RoomPage data={data.data} />);
-    screen.debug();
-    // Checking if each data piece is rendering correctly
-    data.data.forEach(item => {
-      const selector = `h1[data-testid="${item.device.room_number}-co2"]`;
-      // console.log(selector)
-      const co2Element = container.querySelector(selector);
-      // console.log(co2Element)
-      expect(co2Element).toBeInTheDocument();
-      console.log(`${item.device.room_number} CO2 Level is ${item.co2}`);
-      expect(co2Element.textContent).toContain(`${item.device.room_number} CO2 Level is ${item.co2}`);
-    });
-    
+    // Renders the RoomPage component within a MemoryRouter that has route parameter
+    render(
+      <MemoryRouter initialEntries={['/rooms/D201']}>
+        <Routes>
+          <Route path="/rooms/:roomNumber" element={<RoomPage data={data} />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    // Checking if the CO2 element is rendered correctly
+    const co2Element = screen.getByTestId('D201-co2');
+    expect(co2Element).toBeInTheDocument();
+    console.log(co2Element.textContent)
+    expect(co2Element.textContent).toContain('D201 CO2 Level is 2040');
   });
 });
