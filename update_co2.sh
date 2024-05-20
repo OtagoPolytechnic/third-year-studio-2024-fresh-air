c9811c19d8b7d2c559cba4c881093f105a625983
Ubuntu <IoTData@TestServerIoT.5sf2hktcstuebnwzvtv5mt2aia.bx.internal.cloudapp.net>
2024-05-20 01:23:18 +0000

    testing: update should change branch if right input

--- update_co2.sh
+++ update_co2.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
-echo Stopping docker containers if running.
-sudo docker compose down
+run_script=true
 
-echo Removing any images for co2-app.
-sudo docker rmi co2-nginx:latest postgres:16.2 frontend-co2-app:latest backend-co2-app:latest
+what_git_repo () {
+	echo Welcome to the deploy for Co2-app.
+	echo What branch do you want to deploy from?
+	echo 1: main.
+	echo 2: staging.
+	echo 3: other.
+	read -p ": " git_branch
+}
 
-echo Updating repo.
-git stash
-git checkout staging
-git pull
+set_repo () {
+	what_git_repo
+	git checkout ${git_branch}
+	echo ${git_branch}
+}
 
-echo Creating and Running container.
-sudo docker compose up -d && echo Deploy Successful || echo Deploy Failed
+set_repo
+
+#echo Stopping docker containers if running.
+#sudo docker compose down
+
+#echo Removing any images for co2-app.
+#sudo docker rmi co2-nginx:latest postgres:16.2 frontend-co2-app:latest backend-co2-app:latest
+
+#echo Updating repo.
+#git stash
+#git checkout staging
+#git pull
+
+#echo Creating and Running container.
+#sudo docker compose up -d && echo Deploy Successful || echo Deploy Failed
