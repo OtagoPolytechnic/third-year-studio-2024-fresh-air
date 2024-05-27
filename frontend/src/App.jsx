2bf3f50f042695d49eced64979de01a51ae0150a
tracy macaulay-higgan <tracy@mad.geek.nz>
2024-05-27 18:36:09 +1200

    changed so that the body takes up the full height of the viewport

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ const navbarContent = (
     <ul className="flex space-x-4 mt-2 md:flex-col md:space-x-0 md:space-y-4">
       <li><a href="/" className="hover:text-blue-300">Home</a></li>
       <li><a href="/SensorHistory" className="hover:text-blue-300">Sensor History</a></li>
-      <li><a href="/D-Block/207" className="hover:text-blue-300">Krissi's Room</a></li>
+      <li><a href="/D-Block/D207" className="hover:text-blue-300">Krissi's Room</a></li>
     </ul>
   </>
 );
@@ -39,7 +39,7 @@ const bodyContent = (
 export const App = () => {
   return (
     <>
-      return <Layout navbar={navbarContent} body={bodyContent} />;
+    return <Layout navbar={navbarContent} body={bodyContent} />;
     </>
   );
 };
--- frontend/src/Component/Layout.jsx
+++ frontend/src/Component/Layout.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 const Layout = ({ navbar, body }) => {
   return (
-    <div className="font-sans antialiased text-gray-900">
-      <nav className="bg-blue-500 text-white w-full p-4 flex items-center fixed top-0 
-                      md:w-auto md:h-full md:fixed md:left-0 md:flex-col">
-        <div className="container mx-auto md:flex md:flex-col">
-          {navbar}
-        </div>
+    <div id="root" className="h-full flex flex-col">
+      <nav
+        className="bg-blue-500 text-white w-full p-4 flex items-center fixed top-0 
+                 md:w-auto md:h-full md:fixed md:left-0 md:flex-col"
+      >
+        <div className="container mx-auto md:flex md:flex-col">{navbar}</div>
       </nav>
-      <main className="mt-20 p-4 md:ml-40">
-        <div className="container mx-auto">
-          {body}
-        </div>
+      <main className="mt-20 p-4 md:ml-40 flex-1 overflow-y-auto">
+        <div className="container mx-auto">{body}</div>
       </main>
     </div>
   );
