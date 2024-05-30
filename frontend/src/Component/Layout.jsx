6e435e923a86f9d6bae23885b6a05c7868f5cd0b
tracy macaulay-higgan <tracy@mad.geek.nz>
2024-05-30 13:17:03 +1200

    added the z index and  it means it scrolls better

--- frontend/src/Component/Layout.jsx
+++ frontend/src/Component/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ navbar, body }) => {
     <div id="root" className="h-full flex flex-col">
       <nav
         className="bg-blue-500 text-white w-full p-4 flex items-center fixed top-0 
-                 md:w-auto md:h-full md:fixed md:left-0 md:flex-col"
+                 md:w-auto md:h-full md:fixed md:left-0 md:flex-col z-10"
       >
         <div className="container mx-auto md:flex md:flex-col">{navbar}</div>
       </nav>
