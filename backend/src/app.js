51a21c9feecb803ed0422ab434e7348dee4b7213
DSmith575 <SMITDE5@student.op.ac.nz>
2024-06-08 13:41:17 +1200

    feat(websocket): testing out websocket connections

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
+import {WebSocketServer} from 'ws';
+import { PrismaClient } from '@prisma/client';
+
 
 import { INDEX_PATHS, PORTS } from './utils/constants/globalConstants.js';
 import { STATUS_CODES } from './utils/statusCodes/statusCode.js';
@@ -10,6 +13,7 @@ import payload from './routes/sensorData/sensorData.route.js';
 import device from './routes/devices/device.route.js';
 import block from './routes/blocks/block.route.js';
 
+const prisma = new PrismaClient();
 const port = PORTS.SERVER_PORT;
 // basePath sets up the /api/v1 endpoint
 const basePath = `/${INDEX_PATHS.BASE_URL}/${INDEX_PATHS.CURRENT_VERSION}`;
@@ -43,8 +47,41 @@ app.get('/', (req, res) => {
   });
 });
 
+
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+const wss = new WebSocketServer({ server });
+
+const fetchDataFromDatabase = async () => {
+  try {
+    const data = await prisma.sensorData.findMany(); 
+
+      wss.clients.forEach((client) => {
+        if (client.readyState === client.OPEN) {
+          client.send(JSON.stringify(data));
+        }
+      });
+
+  } catch (error) {
+    console.error('Error fetching data from the database:', error);
+  }
+};
+
+// Fetch data from the database every 5 minutes
+setInterval(fetchDataFromDatabase, 1000); // 5 minutes in milliseconds
+
+// WebSocket connection handling
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+
+
 
 export { app, server };
