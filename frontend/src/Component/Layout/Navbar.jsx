ffd42fb9aafb16aa5f2ef0603685065159fb8834
brittbrink <britt.brink@gmail.com>
2024-07-29 15:03:18 +1200

    style(React): changing navbar from side to top

--- frontend/src/Component/Layout/Navbar.jsx
+++ frontend/src/Component/Layout/Navbar.jsx
@@ -4,8 +4,8 @@ import { routes } from "../../router/routeList";
 const NavBar = () => {
   return (
     <>
-      <nav className="h-16 w-screen bg-blue-500 fixed z-10 text-white flex justify-center items-center lg:items-start lg:w-20 lg:h-full">
-        <ul className="flex lg:flex-col">
+      <nav className="h-16 w-screen bg-blue-500 fixed z-10 text-white flex text-center ">
+        <ul className="flex">
           {routes.map((route, index) => (
             <li key={index} className="px-4 lg:pb-4 font-bold hover:underline">
               <NavLink to={route.path}>{route.label}</NavLink>
