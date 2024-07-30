127afe81af2855a2bf3519f18ff5dcfa48a38d2f
brittbrink <britt.brink@gmail.com>
2024-07-30 14:34:30 +1200

    feat(React): added image to navbar

--- frontend/src/Component/Layout/Navbar.jsx
+++ frontend/src/Component/Layout/Navbar.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { routes } from "../../router/routeList";
+import logo from "../../Imgs/logo.png";
 
 const NavBar = () => {
   return (
     <>
       <nav className="h-16 w-screen bg-blue-500 fixed z-10 text-white flex text-center ">
+        <img src={logo} alt="Logo"></img>
         <ul className="flex">
           {routes.map((route, index) => (
             <li key={index} className="px-4 lg:pb-4 font-bold hover:underline">
