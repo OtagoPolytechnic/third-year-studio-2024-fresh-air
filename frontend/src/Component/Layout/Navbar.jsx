2ccc6918253f981cedb9a487de4c6c0bc28514ff
brittbrink <britt.brink@gmail.com>
2024-08-01 14:28:11 +1200

    feat(navbar.jsx): i have added the logo
    I also added some styling for the navigation bar
    
    #150

--- frontend/src/Component/Layout/Navbar.jsx
+++ frontend/src/Component/Layout/Navbar.jsx
@@ -5,11 +5,11 @@ import logo from "../../Imgs/logo.png";
 const NavBar = () => {
   return (
     <>
-      <nav className="h-16 w-screen bg-blue-500 fixed z-10 text-white flex text-center ">
-        <img src={logo} alt="Logo"></img>
-        <ul className="flex">
+      <nav className="h-16 w-screen bg-blue-500 fixed z-10 text-white flex">
+        <img class="ml-2 mt-2 object-center h-12" src={logo} alt="Logo"></img>
+        <ul className="ml-2 flex items-center">
           {routes.map((route, index) => (
-            <li key={index} className="px-4 lg:pb-4 font-bold hover:underline">
+            <li key={index} className="px-2 lg:pb-1 font-bold hover:underline">
               <NavLink to={route.path}>{route.label}</NavLink>
             </li>
           ))}
