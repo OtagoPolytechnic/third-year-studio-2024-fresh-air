1cdaa69560d37742fb0ff1568b0713879e993254
DSmith575 <SMITDE5@student.op.ac.nz>
2024-08-04 12:03:27 +1200

    feat(component): removed api end point list from app.js (backend)

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,22 +28,6 @@ app.use(`${basePath}/rooms`, payload);
 app.use(`${basePath}/devices`, device);
 app.use(`${basePath}/blocks`, block);
 
-app.get('/', (req, res) => {
-  return res.status(STATUS_CODES.OK).json({
-    statusCode: res.statusCode,
-    message: 'Available endpoints',
-    endpoints: {
-      api_path: `${basePath}`,
-      webhook: `[POST]: ${basePath}/integrations/webhook`,
-      all_device_info: `[GET]: ${basePath}/devices`,
-      single_device_info: `[GET]: ${basePath}/devices/{dev_eui}`,
-      device_history_info: `[GET]: ${basePath}/rooms/history/{dev_eui}`,
-      recent_room_data: `[GET]: ${basePath}/rooms/latest/{dev_eui}`,
-      all_room_data: `[GET]: ${basePath}/rooms/{dev_eui}`,
-    },
-  });
-});
-
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
