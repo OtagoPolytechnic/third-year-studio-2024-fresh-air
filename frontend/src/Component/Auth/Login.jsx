846ef7d693ee4814337dc4806bd6cd4c3ff3d221
DSmith575 <SMITDE5@student.op.ac.nz>
2024-09-15 22:42:49 +1200

    (feat)accounts: Added firebase SDK and routing to backend, added registering new users to frontend

--- frontend/src/Component/Auth/Login.jsx
+++ frontend/src/Component/Auth/Login.jsx
@@ -1,27 +1,11 @@
-import { useEffect, useState } from 'react';
-import {
-  useUserAuth
-} from '../../Context/FirestoreAuthContext';
+import { useState } from 'react';
+import { useUserAuth } from '../../Context/FirestoreAuthContext';
 import PopUp from './PopUp';
 import Logout from './Logout';
-import { getUserList } from '../../utils/firestoreFunctions/firestoreFunctions';
 
 export const Login = () => {
   const { login, user } = useUserAuth();
   const [modal, setModal] = useState(false);
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const userList = await getUserList({ collectionName: 'users' });
-        setUsers(userList);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchUsers();
-  }, []);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -38,67 +22,60 @@ export const Login = () => {
     setModal(false);
   };
 
-
-
   return (
-    <div class="flex min-h-full flex-col justify-center px-10 py-8 lg:mx-64 md:mr-14 md:mx-14 border rounded-lg mx-4 mr-4 shadow-lg">
-      <div class="mx-auto w-full max-w">
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Log in to your account
-        </h2>
-      </div>
-      <div class="mt-7 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-2" onSubmit={handleLogin} >
-          <label
-            for="email"
-            class="block text-sm font-medium leading-6 text-gray-900"
-          >
-            E-mail address
-          </label>
-          <input
-            required
-            type="text"
-            name="email"
-            id="email"
-            placeholder="E-mail address"
-            class="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-400 sm:text-sm sm:leading-6"
-          />
-          <label
-            for="pwd"
-            class="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Password
-          </label>
-          <input
-            required
-            type="password"
-            name="pwd"
-            id="pwd"
-            placeholder="Password"
-            class="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-400 sm:text-sm sm:leading-6"
-          />
-          {/* <p>{user ? `Logged in as ${user}` : 'Not logged in'}</p> */}
-          {!user  && (
-          <button
-            disabled={user}
-            type={'submit'}
-            class="flex w-full justify-center rounded-md bg-slate-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Login
-          </button>
-
-          )}
-        </form>
-        <Logout/>
-      </div>
-      {users.map((user) => (
-        <div key={user.userId}>
-          <p>{user.userId}</p>
-          <p>{user.role}</p>
+    <>
+      <div class="flex min-h-full flex-col justify-center px-10 py-8 lg:mx-64 md:mr-14 md:mx-14 border rounded-lg mx-4 mr-4 shadow-lg">
+        <div class="mx-auto w-full max-w">
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Log in to your account
+          </h2>
         </div>
-      ))}
-      {modal && <PopUp handleClick={handleModal}  />}
-    </div>
+        <div class="mt-7 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form class="space-y-2" onSubmit={handleLogin}>
+            <label
+              for="email"
+              class="block text-sm font-medium leading-6 text-gray-900"
+            >
+              E-mail address
+            </label>
+            <input
+              required
+              type="text"
+              name="email"
+              id="email"
+              placeholder="E-mail address"
+              class="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-400 sm:text-sm sm:leading-6"
+            />
+            <label
+              for="pwd"
+              class="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password
+            </label>
+            <input
+              required
+              type="password"
+              name="pwd"
+              id="pwd"
+              placeholder="Password"
+              class="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-400 sm:text-sm sm:leading-6"
+            />
+            {/* <p>{user ? `Logged in as ${user}` : 'Not logged in'}</p> */}
+            {!user && (
+              <button
+                disabled={user}
+                type={'submit'}
+                class="flex w-full justify-center rounded-md bg-slate-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </button>
+            )}
+          </form>
+          <Logout />
+        </div>
+        {modal && <PopUp handleClick={handleModal} headerText={'Successfully logged in'} pText={'Account has been successfully logged in, you can now access the dashboard.'} />}
+      </div>
+    </>
   );
 };
 
--- frontend/src/utils/firestoreFunctions/firestoreFunctions.js
+++ frontend/src/utils/firestoreFunctions/firestoreFunctions.js
@@ -1,16 +1,67 @@
-import { firestore } from "../../firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { firestore } from '../../firebase';
+import { collection, getDocs, doc, setDoc } from 'firebase/firestore';
+import { firestoreCollectionUsers } from '../constants/constants';
+
+const apiKey = import.meta.env.VITE_BACKEND_API_KEY;
 
 export const getUserList = async ({ collectionName }) => {
-    try {
-      const userList = collection(firestore, collectionName);
-      const snapshot = await getDocs(userList);
-      const userDocs = snapshot.docs.map((doc) => {
-        return { userId: doc.id, role: doc.data().role, firstName: doc.data().firstName, lastName: doc.data().lastName };
-      })
-      console.log(userDocs);
-      return userDocs;
-    } catch (error) {
-      throw error;
+  try {
+    const userList = collection(firestore, collectionName);
+    const snapshot = await getDocs(userList);
+    const userDocs = snapshot.docs.map((doc) => {
+      return doc.data();
+    });
+    console.log(userDocs);
+    return userDocs;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const registerUser = async (email, password, role, firstName, lastName) => {
+  try {
+    const response = await fetch(`${apiKey}/api/v1/createUser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || 'Failed to create user');
     }
-  };
+
+    const { uid } = await response.json();
+
+    await createUserInformation({
+      userID: uid,
+      email,
+      role,
+      firstName,
+      lastName,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createUserInformation = async ({
+  userID, email, role, firstName, lastName
+}) => {
+  try {
+    const newUser = doc(firestore, firestoreCollectionUsers, userID);
+    await setDoc(newUser, {
+      email,
+      role,
+      firstName,
+      lastName,
+    });
+  } catch (error) {
+    console.error('Error creating user:', error);
+  }
+}
