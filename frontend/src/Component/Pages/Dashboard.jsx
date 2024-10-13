04032c06e6c5251557d008efed9fd201809f554f
DSmith575 <SMITDE5@student.op.ac.nz>
2024-10-13 19:07:50 +1300

    (fix)component: Uncommented Register in dashboard

--- frontend/src/Component/Pages/Dashboard.jsx
+++ frontend/src/Component/Pages/Dashboard.jsx
@@ -12,7 +12,7 @@ const DashBoard = () => {
     <section className="lg:mx-16 mx-4 justify-center items-center grid grid-cols-2 lg:grid-cols-6 grid-rows-3">
       <AddSensor styles={'lg:col-start-1 lg:col-span-2 col-start-1 col-span-2'} />
       <UpdateSensor styles={'lg:col-start-4 lg:row-start-1 lg:col-span-3 col-start-1 col-span-3'} />
-      {/* <Register styles={'lg:col-start-1 lg:row-start-2 lg:col-span-3 col-start-1 col-span-full'} /> */}
+      <Register styles={'lg:col-start-1 lg:row-start-2 lg:col-span-3 col-start-1 col-span-full'} />
       <CreateBlock styles={'lg:col-start-4 lg:row-start-4 lg:col-span-2 col-start-1 col-span-3'} />
       <UpdateBlock styles={'lg:col-start-4 lg:row-start-3 lg:col-span-2 col-start-1 col-span-3'}/>
       <AddDeviceToBlock styles={'lg:col-start-4 lg:row-start-2 lg:col-span-2 col-start-1 col-span-3'}/>
