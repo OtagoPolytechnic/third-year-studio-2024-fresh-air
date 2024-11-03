4f885254bfb92a31619c30669028668872a54201
brittbrink <britt.brink@gmail.com>
2024-11-04 12:47:05 +1300

    style(homepage.jsx): removed the darkmode from the blocks, it will now always display the same style
    #252

--- frontend/src/Component/Pages/Homepage.jsx
+++ frontend/src/Component/Pages/Homepage.jsx
@@ -13,7 +13,7 @@ export const Homepage = () => {
           {blocks.map((block) => (
             <div
               key={block.blockName}
-              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
             >
               <li className='text-center'>
                 <NavLink
